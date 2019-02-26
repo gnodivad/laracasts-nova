615e9965566af5c5efbf851c14ce5ff310df8613
David <dodwei.dev@gmail.com>
2019-02-26 16:03:53 +0800

    Resource validation

--- app/Nova/Post.php
+++ app/Nova/Post.php
@@ -48,16 +48,31 @@ class Post extends Resource
     {
         return [
             ID::make()->sortable(),
-            Text::make('Title'),
-            Trix::make('Body'),
-            DateTime::make('Publish At')->hideFromIndex(),
-            DateTime::make('Publish Until')->hideFromIndex(),
+
+            Text::make('Title')->rules('required'),
+
+            Trix::make('Body')->rules('required'),
+
+            DateTime::make('Publish At')
+                ->hideFromIndex()
+                ->rules('after_or_equal:today'),
+
+            DateTime::make('Publish Until')
+                ->hideFromIndex()
+                ->rules('after_or_equal:publish_at'),
+
             Boolean::make('Is Published'),
-            Select::make('Category')->options([
-               'tutorials' => 'Tutorials',
-               'news' => 'News'
-            ])->hideWhenUpdating(),
+
+            Select::make('Category')
+                ->options([
+                   'tutorials' => 'Tutorials',
+                   'news' => 'News'
+                ])
+                ->hideWhenUpdating()
+                ->rules('required'),
+
             BelongsTo::make('User'),
+
             BelongsToMany::make('Tags')
         ];
     }
