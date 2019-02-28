6fb02e1b617e4c0aa3af0165d991c1dfac034535
David <dodwei.dev@gmail.com>
2019-02-28 16:14:07 +0800

    Create a custom tool

---
+++ nova-components/Viewcache/resources/js/components/Tool.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <heading class="mb-6">View Cache</heading>
+
+        <card class="flex flex-col items-center justify-center" style="min-height: 300px">
+            <h1 class="text-black text-4xl text-90 font-light mb-6">Enable your Application View Cache</h1>
+
+            <button @click="activateCache" class="btn btn-primary btn-default">Enable Cache</button>
+        </card>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        activateCache() {
+            Nova.request().post('/nova-vendor/viewcache')
+                .then(data => {
+                   alert("Success");
+                });
+        }
+    },
+}
+</script>
+
+<style>
+/* Scoped Styles */
+</style>
